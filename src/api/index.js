8f1b8f631075fd180374b724c04eb086ad545f23
Srijan Sharma <srijans38@gmail.com>
2020-04-30 04:33:06 +0530

    URL Input

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 var key = process.env.VUE_APP_GOOGLE;
-var url = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=PLdTodMosi-BwEwlzjN6EyS1vwGXFo-UlK&part=snippet&key=${key}&fields=items(snippet(publishedAt,title,description,thumbnails(medium),position,resourceId(videoId)))&maxResults=50`
 
-export const getPlaylistData = async () => {
+export const getPlaylistDataAPI = async (listId) => {
     try {
+        var url = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${listId}&part=snippet&key=${key}&fields=items(snippet(publishedAt,title,description,thumbnails(medium),position,resourceId(videoId)))&maxResults=50`
         const response = await axios.get(url);
         return response;
     } catch (error) {
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,15 +4,19 @@
       <b-col class="text-center">
         <h3 class="my-5">Youtube Playlist Calculator</h3>
         <hr>
-        <b-button class="my-5" @click="getPlaylistData">Test</b-button>
+        <b-input-group prepend="URL" class="my-5">
+          <b-form-input v-model="playlistUrl" placeholder="https://www.youtube.com/playlist?list=PL6rBC_87VKs76V9Vc0s44Gg_Q6qObZrFY"></b-form-input>
+          <b-input-group-append>
+            <b-button @click="parseAndGo" variant="outline-danger">Go</b-button>
+          </b-input-group-append>
+        </b-input-group>
       </b-col>
     </b-row>
     <hr>
     <transition-group name="list" tag="div" class="row mb-5">
       <b-col class="text-center" sm="12" md="6" lg="4" v-for="item in playlistItems"
       :key="item.position">
-          <b-card  
-
+          <b-card
           :img-src="item.thumbUrl"
           img-alt="Image"
           img-top
@@ -20,7 +24,7 @@
           style=""
           class="mt-5 mx-2"
           >
-            <b-card-title class="text-truncate"> {{ item.title }}</b-card-title>
+            <b-card-title class="text-truncate"> {{item.position+1 + '. ' + item.title }}</b-card-title>
             <b-card-text class="text-truncate">
               {{ item.description }}
             </b-card-text>
@@ -33,21 +37,39 @@
 </template>
 
 <script>
-import { getPlaylistData } from "@/api";
+import { getPlaylistDataAPI } from "@/api";
 export default {
   name: "Home",
   data() {
     return {
-      playlistItems: []
+      playlistItems: [],
+      playlistUrl: '',
     };
+  },
+  computed: {
+    playlistId() {
+      var { list } = this.parseURL(this.playlistUrl);
+      return list;
+    }
   },
   methods: {
     async getPlaylistData() {
       const {
         data: { items }
-      } = await getPlaylistData();
+      } = await getPlaylistDataAPI(this.playlistId);
       items.map((item) => {
-        const {snippet : {description, position, publishedAt, resourceId : { videoId }, thumbnails : { medium : { url }}, title}} = item;
+        const { 
+          snippet : { 
+            description, 
+            position, 
+            publishedAt, 
+            resourceId : { videoId }, 
+            thumbnails : { 
+              medium : { url }
+            }, 
+            title
+          }
+        } = item;
         this.playlistItems.push({
           description,
           position,
@@ -57,6 +79,24 @@ export default {
           title
         });
       });
+    },
+    parseURL(url) {
+      var parser = document.createElement('a'),
+        searchObject = {},
+        queries, split, i;
+      parser.href = url;
+      queries = parser.search.replace(/^\?/, '').split('&');
+      for( i = 0; i < queries.length; i++ ) {
+        split = queries[i].split('=');
+        searchObject[split[0]] = split[1];
+      }
+      return searchObject;
+    },
+    parseAndGo() {
+      if(this.playlistId) {
+        console.log('test');
+        this.getPlaylistData();
+      }
     }
   }
 };
@@ -64,11 +104,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.list-enter-active, .list-leave-active {
-  transition: all 1s;
-}
-.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
-  opacity: 0;
-  transform: translateY(30px);
-}
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    transform: translateY(30px);
+  }
 </style>
