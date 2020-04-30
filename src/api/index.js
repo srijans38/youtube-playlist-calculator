19447244567d9c89bcaaa75e2be0cf28bb943e77
Srijan Sharma <srijans38@gmail.com>
2020-05-01 05:19:15 +0530

    Handle API Pagination

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 var key = process.env.VUE_APP_GOOGLE;
 
-export const getPlaylistDataAPI = async (listId) => {
+export const getPlaylistDataAPI = async (listId, nextPageToken) => {
     try {
-        var url = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${listId}&part=snippet&key=${key}&fields=items(snippet(publishedAt,title,description,thumbnails(medium),position,resourceId(videoId)))&maxResults=50`
+        var url = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${listId}&part=snippet&key=${key}&fields=nextPageToken,items(snippet(publishedAt,title,description,thumbnails(medium),position,resourceId(videoId)))&maxResults=50&pageToken=${nextPageToken}`
         const response = await axios.get(url);
         return response;
     } catch (error) {
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,34 +3,45 @@
     <b-row align-h="center" align-content="center">
       <b-col class="text-center">
         <h3 class="my-5">Youtube Playlist Calculator</h3>
-        <hr>
+        <hr />
         <b-input-group prepend="URL" class="my-5">
-          <b-form-input v-model="playlistUrl" placeholder="https://www.youtube.com/playlist?list=PL6rBC_87VKs76V9Vc0s44Gg_Q6qObZrFY"></b-form-input>
+          <b-form-input
+            v-model="playlistUrl"
+            placeholder="https://www.youtube.com/playlist?list=PL6rBC_87VKs76V9Vc0s44Gg_Q6qObZrFY"
+          ></b-form-input>
           <b-input-group-append>
             <b-button @click="parseAndGo" variant="outline-danger">Go</b-button>
+            <b-button @click="getPlaylistData" variant="outline-danger">Next</b-button>
           </b-input-group-append>
         </b-input-group>
       </b-col>
     </b-row>
-    <hr>
+    <hr />
     <transition-group name="list" tag="div" class="row mb-5">
-      <b-col class="text-center" sm="12" md="6" lg="4" v-for="item in playlistItems"
-      :key="item.position">
-          <b-card
+      <b-col
+        class="text-center"
+        sm="12"
+        md="6"
+        lg="4"
+        v-for="item in playlistItems"
+        :key="item.position"
+      >
+        <b-card
           :img-src="item.thumbUrl"
           img-alt="Image"
           img-top
           tag="article"
-          style=""
+          style
           class="mt-5 mx-2"
-          >
-            <b-card-title class="text-truncate"> {{item.position+1 + '. ' + item.title }}</b-card-title>
-            <b-card-text class="text-truncate">
-              {{ item.description }}
-            </b-card-text>
+        >
+          <b-card-title class="text-truncate">{{item.position+1 + '. ' + item.title }}</b-card-title>
+          <b-card-text class="text-truncate">{{ item.description }}</b-card-text>
 
-            <b-button :href="'https://youtube.com/watch?v='+ item.videoId" variant="primary">Watch Video</b-button>
-          </b-card>
+          <b-button
+            :href="'https://youtube.com/watch?v='+ item.videoId"
+            variant="primary"
+          >Watch Video</b-button>
+        </b-card>
       </b-col>
     </transition-group>
   </b-container>
@@ -43,7 +54,9 @@ export default {
   data() {
     return {
       playlistItems: [],
-      playlistUrl: '',
+      playlistUrl:
+        "https://www.youtube.com/playlist?list=PLQVvvaa0QuDe8XSftW-RAxdo6OmaeL85M",
+      nextPageToken: ""
     };
   },
   computed: {
@@ -55,18 +68,18 @@ export default {
   methods: {
     async getPlaylistData() {
       const {
-        data: { items }
-      } = await getPlaylistDataAPI(this.playlistId);
-      items.map((item) => {
-        const { 
-          snippet : { 
-            description, 
-            position, 
-            publishedAt, 
-            resourceId : { videoId }, 
-            thumbnails : { 
-              medium : { url }
-            }, 
+        data: { nextPageToken, items }
+      } = await getPlaylistDataAPI(this.playlistId, this.nextPageToken);
+      items.map(item => {
+        const {
+          snippet: {
+            description,
+            position,
+            publishedAt,
+            resourceId: { videoId },
+            thumbnails: {
+              medium: { url }
+            },
             title
           }
         } = item;
@@ -75,26 +88,34 @@ export default {
           position,
           publishedAt,
           videoId,
-          thumbUrl : url,
+          thumbUrl: url,
           title
         });
       });
+      console.log(nextPageToken);
+      this.nextPageToken = nextPageToken;
+      if (this.nextPageToken && this.nextPageToken != "") {
+        this.getPlaylistData();
+      }
     },
     parseURL(url) {
-      var parser = document.createElement('a'),
+      var parser = document.createElement("a"),
         searchObject = {},
-        queries, split, i;
+        queries,
+        split,
+        i;
       parser.href = url;
-      queries = parser.search.replace(/^\?/, '').split('&');
-      for( i = 0; i < queries.length; i++ ) {
-        split = queries[i].split('=');
+      queries = parser.search.replace(/^\?/, "").split("&");
+      for (i = 0; i < queries.length; i++) {
+        split = queries[i].split("=");
         searchObject[split[0]] = split[1];
       }
       return searchObject;
     },
     parseAndGo() {
-      if(this.playlistId) {
-        console.log('test');
+      this.playlistItems = [];
+      this.nextPageToken = "";
+      if (this.playlistId) {
         this.getPlaylistData();
       }
     }
@@ -104,11 +125,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .list-enter-active, .list-leave-active {
-    transition: all 1s;
-  }
-  .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
-    opacity: 0;
-    transform: translateY(30px);
-  }
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
